443b842d136228feff02c4327f77a7b12d6dec4e
LukasMatias <lukaskt6@gmail.com>
2021-08-09 11:32:31 -0300

    Initial commit

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var name: String  = "Lukas"
+    var cpf: String = "085.448.764.00"
+}
+ fun main(){
+     val test = Pessoa()
+
+     println(test.name)
+     println(test.cpf)
+ }
